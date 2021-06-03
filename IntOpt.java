86348ca5d91e5b8229845173a101d11f602d6186
InduLekha <rindulekha09.com>
2021-06-03 21:27:49 +0530

    IntOpt

---
+++ IntOpt.java
@@ -0,0 +1,20 @@
+public class IntOpt {
+		  public static void main(String[] args) {
+		    
+		   
+		    int a = 12, b = 5, c=10;
+
+		   
+		    System.out.println("a + b *c = " + (a + b *c));
+
+		   
+		    System.out.println("a * b + c = " + (a * b + c));
+
+		   
+		    System.out.println("c + a / b  = " + (c + a / b));
+
+		    
+		    System.out.println("a % b + c  = " + (a % b + c));
+
+		  }
+		}
