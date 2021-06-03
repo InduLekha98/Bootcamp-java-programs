91d1470e062827f6e8b6e58d665eae23a5879edb
InduLekha <rindulekha09.com>
2021-06-03 22:05:26 +0530

    DoubleOpt

---
+++ DoubleOpt.java
@@ -0,0 +1,21 @@
+public class DoubleOpt {
+	  public static void main(String[] args) {
+		    
+		   
+		    double a = 5.5, b = 7.8, c=56.34;
+
+		   
+		    System.out.println("a + b *c = " + (a + b *c));
+
+		   
+		    System.out.println("a * b + c = " + (a * b + c));
+
+		   
+		    System.out.println("c + a / b  = " + (c + a / b));
+
+		    
+		    System.out.println("a % b + c  = " + (a % b + c));
+
+		  }
+
+}
