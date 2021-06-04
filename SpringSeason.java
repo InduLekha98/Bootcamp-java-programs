e4f3e8ad9524f5561e1c6a0dbef5e2171ab733ff
InduLekha <rindulekha09.com>
2021-06-04 11:09:25 +0530

    SpringSeason

---
+++ SpringSeason.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SpringSeason {
+	
+
+	public static void main(String[] args) {
+		
+		int month, day;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter an Month here : ");
+		month = sc.nextInt();
+		System.out.println("Enter an Day here : ");
+		day=sc.nextInt();
+    boolean isSpring =  (month == 3 && day >= 20 && day <= 31)
+                     || (month == 4 && day >=  1 && day <= 30)
+                     || (month == 5 && day >=  1 && day <= 31)
+                     || (month == 6 && day >=  1 && day <= 20);
+                  
+    System.out.println(isSpring);
+   
+}
+
+}
