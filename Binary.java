1f81512b969e424a0618eeab2319882282479f78
InduLekha <rindulekha09.com>
2021-06-09 00:53:11 +0530

    conversion from decimal to binary using swapping

--- Binary.java
+++ Binary.java
@@ -22,11 +22,19 @@ public class Binary {
             System.out.print(binaryNum[j]);
     }
  
+	static int swapNibbles(int x)
+	{
+	    return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+	    
+	    
+	}
     
     public static void main(String[] args)
     {
         int n = 9;
         decToBinary(n);
+        int x = 100;
+	    System.out.print("	"+swapNibbles(x));
+	    
     }
 }
- 
