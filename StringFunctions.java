727b2c04c1a341fd62247a3529108c6ca10d2daf
InduLekha <rindulekha09.com>
2021-06-10 23:47:13 +0530

    given String is palindrome or not

--- StringFunctions.java
+++ StringFunctions.java
@@ -1,29 +1,21 @@
 package com.bridgeLabz.boosterbootcampjavaprogram.Day4;
-import java.util.Arrays;
 
-public class AnagramString {
-	   static void isAnagram(String str1, String str2) {  
-	        String s1 = str1.replaceAll("\\s", "");  
-	        String s2 = str2.replaceAll("\\s", "");  
-	        boolean status = true;  
-	        if (s1.length() != s2.length()) {  
-	            status = false;  
-	        } else {  
-	            char[] ArrayS1 = s1.toLowerCase().toCharArray();  
-	            char[] ArrayS2 = s2.toLowerCase().toCharArray();  
-	            Arrays.sort(ArrayS1);  
-	            Arrays.sort(ArrayS2);  
-	            status = Arrays.equals(ArrayS1, ArrayS2);  
-	        }  
-	        if (status) {  
-	            System.out.println(s1 + " and " + s2 + " are anagrams");  
-	        } else {  
-	            System.out.println(s1 + " and " + s2 + " are not anagrams");  
-	        }  
-	    }  
-	   
-	    public static void main(String[] args) {  
-	        isAnagram("heart", "earth"); 
+public class Palindrome {
+	public static void main(String args[]){  
+		  int r,sum=0,temp;    
+		  int n=545;  
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;    
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");    
+		}  
+		}  
 
-	    }
-}
+
