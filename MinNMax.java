7383fecd01cbfa4c54ce4e3065850b810ed61f35
InduLekha <rindulekha09.com>
2021-06-14 02:59:55 +0530

    min and max values using array of strings

---
+++ MinNMax.java
@@ -0,0 +1,25 @@
+package com.bridgeLabz.boosterbootcampjavaprogram.Day5;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MinNMax {
+	 public static void main(String[] args)
+	    {
+	 
+	        
+	        Integer[] num = { 2, 4, 7, 5, 9 };
+	 
+	        
+	        int min = Collections.min(Arrays.asList(num));
+	 
+	        
+	        int max = Collections.max(Arrays.asList(num));
+	 
+	        
+	        System.out.println("Minimum number of array is : "
+	                           + min);
+	        System.out.println("Maximum number of array is : "
+	                           + max);
+	    }
+	}
